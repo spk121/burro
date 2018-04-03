224b67335feee67be90c3e87c991fb98f1fd8c2f
Michael Gran <spk121@yahoo.com>
2018-04-02 20:22:03 -0700

    Add some Cygwin bits I'll need later when I try to port it

---
+++ src/visibility.h
@@ -0,0 +1,35 @@
+/* This header, or versions very much like it, are everywhere.  I
+   think, at this point, this code fragment is basically public
+   domain.  */
+
+#ifndef VISIBILITY_H
+#define VISIBILITY_H
+
+#if defined _WIN32 || defined __CYGWIN__
+  #ifdef BUILDING_DLL
+    #ifdef __GNUC__
+      #define DLL_PUBLIC __attribute__ ((dllexport))
+    #else
+      #define DLL_PUBLIC __declspec(dllexport)
+    #endif
+  #else
+    #ifdef __GNUC__
+      #define DLL_PUBLIC __attribute__ ((dllimport))
+    #else
+      #define DLL_PUBLIC __declspec(dllimport)
+    #endif
+  #endif
+  #define DLL_LOCAL
+#else
+  #if __GNUC__ >= 4
+    #define DLL_PUBLIC __attribute__ ((visibility ("default")))
+    #define DLL_LOCAL  __attribute__ ((visibility ("hidden")))
+  #else
+    #define DLL_PUBLIC
+    #define DLL_LOCAL
+  #endif
+#endif
+
+
+
+#endif
